3471412411dc87e6d39c1b3f72616baddccd9c49
unknown <mystifiedthomas@gmail.com>
2018-10-12 07:43:33 -0500

    Adding features and details

--- RealNews.py
+++ RealNews.py
@@ -3,12 +3,13 @@ import requests, webbrowser
 
 #this function writes the search strings to an html document
 
-def write_to_file(list, terms):
+def write_to_file(list, terms, networks, sterms):
     outfile = open(terms, "w")
-    outfile.write('Here are Links to the Major 6 U.S. News Services, for your topics.')
+    outfile.write('Here are Links to the Major 6 U.S. News Services, in no particular order.<br>')
+    outfile.write('The links will lead you to recent searches for your search terms, ' + sterms + ', at the selected network.')
     outfile.write('<br><br>')
-    for elem in list:
-        wrtstr= '<a href = ' + elem + '>' + elem + '</a> \n <br>'
+    for num2 in range(len(list)):
+        wrtstr= '<a href = ' + list[num2] + '>' + networks[num2] + '</a> \n <br>'
         outfile.write(wrtstr)
     outfile.close()
     webbrowser.open('file:///C:/Users/mysti/thomasoriginalcode/Git/RealNews/' + terms)
@@ -27,15 +28,14 @@ def main():
         addr = input("Please enter a search term: ")
         srchlst.append(addr)
         print("")
+    sterms = srchlst[0] + " and " + srchlst[1]
     serchn = "RealNews_" + srchlst[0] + srchlst[1] + ".html"
+    netwrk = ['abc', 'cbs', 'nbc', 'msnbc', 'foxnews', 'cnn']
     serclst = []
-    serclst.append("https://www.google.com/search?&as_sitesearch=nbc.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    serclst.append("https://www.google.com/search?&as_sitesearch=abc.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    serclst.append("https://www.google.com/search?&as_sitesearch=cbs.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    serclst.append("https://www.google.com/search?&as_sitesearch=msnbc.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    serclst.append("https://www.google.com/search?&as_sitesearch=foxnews.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    serclst.append("https://www.google.com/search?&as_sitesearch=cnn.com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
-    write_to_file(serclst, serchn)
+    for num1 in range(6):
+        serclst.append("https://www.google.com/search?&as_sitesearch=" + netwrk[num1] + ".com&as_q=" + srchlst[0] + "%2C+" + srchlst[1])
+    print("A webpage will now open in your browser with the links you requested.") 
+    write_to_file(serclst, serchn, netwrk, sterms)
 
 
 ## THE GHOST OF THE SHADOW ##
